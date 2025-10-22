c91a483019e7247f45ba403e99629925d425ad0b
brittanyramirez <brittanyramirez@R2Hs-MacBook-Pro.local>
2025-10-22 16:10:28 -0400

    initial commit

---
+++ google.js
@@ -0,0 +1,9 @@
+// this is activating the google search button to work//
+function googleSearch() {
+    const query = document.getElementById("searchBar").value;
+    alert("You Search for: " + query);
+}
+// this is making sure the lucky button is working//
+function luckyButton() {
+    alert("I'm feeling lucky!");
+}
